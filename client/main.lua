aae5a9110a906bbeb3ae0e5f2353b577aecb5188
Nazri <40690481+NazriGenfil@users.noreply.github.com>
2023-09-19 17:56:48 +0700

    adding config variabel

--- client/main.lua
+++ client/main.lua
@@ -3,4 +3,5 @@ local xPlayer = nil
 RegisterNetEvent('esx:playerLoaded')
 AddEventHandler('esx:playerLoaded',function(xPlayer, isNew, skin)
     ESX.PlayerData = xPlayer
+    print(json.encode(xPlayer))
 end)
