604253ea8a49970412d6be33162826cce8ad0e92
Nazri <40690481+NazriGenfil@users.noreply.github.com>
2023-09-20 20:30:31 +0700

    fix typo

--- config.lua
+++ config.lua
@@ -7,7 +7,7 @@ config.location = {
 }
 config.blipSprite = 513
 config.jobname = "pedagang"
-config.maxGrade = 3
+config.maxGrade = 2
 
 config.bossAction = {
     coords = vec3(-1206.93, -891.91, 14.6),
